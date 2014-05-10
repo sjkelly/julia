feec5647a293942ac9bf5677150a3348f7a056cb
Viral B. Shah <viral@mayin.org>
2014-05-10 16:24:59 +0530

    WIP for the generalized eigenvalue problem (#6758)

--- base/linalg/arnoldi.jl
+++ base/linalg/arnoldi.jl
@@ -2,9 +2,12 @@ using .ARPACK
 
 ## eigs
 
-function eigs(A;nev::Integer=6, ncv::Integer=20, which::ASCIIString="LM",
+eigs(A; args...) = eigs(A, :Identity, args...)
+
+function eigs(A, B; nev::Integer=6, ncv::Integer=20, which::ASCIIString="LM",
           tol=0.0, maxiter::Integer=1000, sigma=nothing, v0::Vector=zeros((0,)),
           ritzvec::Bool=true, complexOP::Bool=false)
+
     n = chksquare(A)
     (n <= 6) && (nev = min(n-1, nev))
     ncv = blas_int(min(max(2*nev+2, ncv), n))
@@ -12,7 +15,11 @@ function eigs(A;nev::Integer=6, ncv::Integer=20, which::ASCIIString="LM",
     sym   = issym(A)
     T     = eltype(A)
     cmplx = T<:Complex
-    bmat  = "I"
+    if B == :Identity
+        bmat = "I"
+    else
+        bmat = "G"
+    end
     
     if !isempty(v0)
         length(v0)==n || throw(DimensionMismatch(""))
@@ -21,12 +28,16 @@ function eigs(A;nev::Integer=6, ncv::Integer=20, which::ASCIIString="LM",
         v0=zeros(T,(0,))
     end
 
-    if sigma===nothing # normal iteration
+    if sigma===nothing && B == :Identity  # normal iteration
         mode = 1
         sigma = 0
         linop(x) = A * x
     else # inverse iteration with level shift
-        C = lufact(sigma==0 ? A : A - sigma*eye(A))
+        if B == :Identity
+            C = lufact(sigma==0 ? A : A - sigma*eye(A))
+        else
+            C = lufact(sigma==0 ? A : A - sigma*B)
+        end
         if cmplx
             mode = 3
             linop(x) = C\x
@@ -40,10 +51,16 @@ function eigs(A;nev::Integer=6, ncv::Integer=20, which::ASCIIString="LM",
             end
         end     
     end
+
+    if B == :Identity
+        linopB(x) = x
+    else
+        linopB(x) = B*x
+    end
         
     # Compute the Ritz values and Ritz vectors
     (resid, v, ldv, iparam, ipntr, workd, workl, lworkl, rwork, TOL) = 
-       ARPACK.aupd_wrapper(T, linop, n, sym, cmplx, bmat, nev, ncv, which, tol, maxiter, mode, v0)
+       ARPACK.aupd_wrapper(T, linop, linopB, n, sym, cmplx, bmat, nev, ncv, which, tol, maxiter, mode, v0)
     
     # Postprocessing to get eigenvalues and eigenvectors
     ARPACK.eupd_wrapper(T, n, sym, cmplx, bmat, nev, which, ritzvec, TOL,
--- base/linalg/arpack.jl
+++ base/linalg/arpack.jl
@@ -4,7 +4,7 @@ import ..LinAlg: BlasInt, blas_int, ARPACKException
 
 ## aupd and eupd wrappers 
 
-function aupd_wrapper(T, linop::Function, n::Integer,
+function aupd_wrapper(T, linop::Function, linopB::Function, n::Integer,
                       sym::Bool, cmplx::Bool, bmat::ASCIIString,
                       nev::Integer, ncv::Integer, which::ASCIIString, 
                       tol::Real, maxiter::Integer, mode::Integer, v0::Vector)
@@ -47,11 +47,19 @@ function aupd_wrapper(T, linop::Function, n::Integer,
             naupd(ido, bmat, n, which, nev, TOL, resid, ncv, v, n,
                   iparam, ipntr, workd, workl, lworkl, info)
         end
+
+        # Check for warnings and errors
         if info[1] == 3; warn("try eigs/svds with a larger value for ncv"); end
         if info[1] == 1; warn("maximum number of iterations reached; check nconv for number of converged eigenvalues"); end
         if info[1] < 0; throw(ARPACKException(info[1])); end
-        if (ido[1] != -1 && ido[1] != 1); break; end
-        workd[ipntr[2]+zernm1] = linop(getindex(workd, ipntr[1]+zernm1))
+
+        if (ido[1] == -1 || ido[1] == 1)
+            workd[ipntr[2]+zernm1] = linop(getindex(workd, ipntr[1]+zernm1))
+        elseif ido[1] == 2
+            workd[ipntr[2]+zernm1] = linopB(getindex(workd, ipntr[1]+zernm1))
+        else
+            break
+        end
     end
     
     return (resid, v, n, iparam, ipntr, workd, workl, lworkl, rwork, TOL)
